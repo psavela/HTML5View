2176d9788f80a189df0e689a47a02e9b03266923
psavela <petri.m.savela@gmail.com>
2015-10-28 12:15:31 +0200

    Version update

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("Here we go!!");
 
 /*
@@ -9,15 +10,41 @@ window.onload = function(event) {
 
 // JQUERY $ == constructor
 
-$(document).ready(function() {
+$(document).ready(function () {
     
     console.log("jquery onload triggered");
-    $("#head").css("backround-color", "lightblue").css("padding","20px").css("border-radius","8px");
+    $("#head").css("backround-color", "lightblue").css("padding", "20px").css("border-radius", "8px");
     
     $(".about").text("New text");
     $(".about").html("<b>New text2</b>");
     $("[data-dummy]").html("<p>Hello World<p>");
     
+    var setting = {
+        
+        method: "GET",
+        url: "http://localhost:28017/oma/person/",
+        dataType: "jsonp",
+        jsonp: "jsonp"    
+    };
+    
+    $.ajax(setting).done(function (data) {
+        console.log(data);
+        
+        for (var i = 0; i < data.rows.length; i++) {
+            
+            var html = "<tr>" +
+                       "<td>" + data.rows[i].name + "</td>" +
+                       "<td>" + data.rows[i].address + "</td>" +
+                       "<td>" + data.rows[i].age + "</td>" +
+                       "</tr>";
+            
+           
+            $(html).appendTo("tbody");
+        }
+            
+            
+    });
+
 });
 
 /*
